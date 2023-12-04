a10c3ab7d96c926d146e6720c9f41ee9efcec711
Marcos Santos <131712430+gitmvfs@users.noreply.github.com>
2023-12-04 13:51:28 -0300

    Update request_api.js

--- src/service/request_api.js
+++ src/service/request_api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
  const api = axios.create({
-    baseURL: 'https://api-crud-ecommerce.cyclic.app',
+    baseURL: 'https://fair-rose-gazelle-yoke.cyclic.app',
 })
 
 export default api
