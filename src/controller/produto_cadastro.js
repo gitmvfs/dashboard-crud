c56a680a7fcc759293f9dba68a3d139c5ab18876
marcos santos <marcossantos8002@gmail.com>
2023-12-04 02:53:07 -0300

    feat(produto): feito cadastro, separado controller falta card, visualizar e delete

---
+++ src/controller/produto_cadastro.js
@@ -0,0 +1,39 @@
+import api from "../service/request_api";
+import Swal from "sweetalert2";
+import { Navigate } from "react-router-dom";
+
+const cadastrar_produto = (nome,preco,genero,descricao,tamanhosSelecionados,cor,tipo,urlImage_1,urlImage_2,urlImage_3,categoria) => {
+api
+        .post("/produto", {
+          nome: nome,
+          preco: preco,
+          genero: genero.value,
+          descricao: descricao,
+          tamanhos: tamanhosSelecionados,
+          cor: cor,
+          tipo: tipo.value,
+          linkFoto1: urlImage_1,
+          linkFoto2: urlImage_2,
+          linkFoto3: urlImage_3,
+          categoriaNome: categoria.value,
+        })
+        .then((resultado) => {
+          Swal.fire({
+            title: "Cadastrado com sucesso",
+            icon: "success",
+            confirmButtonText: "OK",
+            timer: 3000,
+          });
+        })
+        .catch((err) => {
+          Swal.fire({
+            title: "Parametros inválidos ou produto já cadastrado.",
+            icon: "warning",
+            confirmButtonText: "OK",
+            timer: 3000, // Tempo em milissegundos (3 segundos)
+            timerProgressBar: true,
+          });
+        });
+    }
+
+export default cadastrar_produto
--- src/views/cadastroProduto/cadastroProduto.js
+++ src/views/cadastroProduto/cadastroProduto.js
@@ -1,31 +1,84 @@
 import "./cadastroProduto.css";
-import React, { useState } from 'react';
-import Select from 'react-select';
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
 import axios from "axios";
+import Swal from "sweetalert2";
 import Header from "../../components/layout/header/headerSemFiltro";
+import api from "../../service/request_api";
+import cadastrar_produto from "../../controller/produto_cadastro";
 import cam from "../../images/icons/Group 75.png"
 
 const CadastroProduto = () => {
-  const opcoes = [
-    { value: 'PP', label: 'PP' },
-    { value: 'P', label: 'P' },
-    { value: 'M', label: 'M' },
-    { value: 'G', label: 'G' },
-    { value: 'GG', label: 'GG' },
-    { value: 'XGG', label: 'XGG' },
+  useEffect(() => {
+    // Função assíncrona para buscar as opções da categoria
+    const fetchCategorias = async () => {
+      try {
+        const response = await api.get("/categoria");
+        // Atualiza o estado com as opções da categoria do servidor
+        const nomesDasCategorias = response.data.map(
+          (categoria) => categoria.nome
+        );
+        setCategoriasOpt(nomesDasCategorias);
+      } catch (error) {
+        console.error("Erro ao obter opções de categoria:", error);
+      }
+    };
+
+    // Chama a função para buscar as opções da categoria quando o componente montar
+    fetchCategorias();
+  }, []); // O array de dependências vazio garante que a solicitação seja feita apenas uma vez, quando o componente montar
+
+  const [tamanho, setTamanho] = useState([]);
+  const [nome, setNome] = useState("");
+  const [preco, setPreco] = useState("");
+  const [cor, setCor] = useState("");
+  const [genero, setGenero] = useState("feminino");
+  const [tipo, setTipo] = useState("");
+  const [fotos, setFotos] = useState([]);
+  const [descricao, setDescricao] = useState("");
+  const [categoria, setCategoria] = useState([]);
+  const [categoriaOpt, setCategoriasOpt] = useState([]);
+
+  const tamanhoOptions = [
+    { value: "PP", label: "PP" },
+    { value: "P", label: "P" },
+    { value: "M", label: "M" },
+    { value: "G", label: "G" },
+    { value: "GG", label: "GG" },
+    { value: "XGG", label: "XGG" },
   ];
 
-  const [selectedOptions, setSelectedOptions] = useState([]);
-  const [nome, setNome] = useState('');
-  const [preco, setPreco] = useState('');
-  const [cor, setCor] = useState('');
-  const [genero, setGenero] = useState('feminino');
-  const [tipo, setTipo] = useState('');
-  const [fotos, setFotos] = useState([]);
-  const [descricao, setDescricao] = useState('');
+  const tipoOptions = [
+    { value: "vestido", label: "Vestido" },
+    { value: "macacão", label: "Macacão" },
+    { value: "calça", label: "Calça" },
+    { value: "blusa", label: "Blusa" },
+    { value: "camisa", label: "Camisa" },
+    { value: "calçado", label: "Calçado" },
+    { value: "blazer", label: "Blazer" },
+    { value: "paletó", label: "Paletó" },
+  ];
 
-  const handleSelectChange = (selected) => {
-    setSelectedOptions(selected);
+  const generoOptions = [
+    { value: "masculino", label: "Masculino" },
+    { value: "feminino", label: "Feminino" },
+    { value: "unissex", label: "Unissex" },
+  ];
+
+  const handleTamanho = (selected) => {
+    setTamanho(selected);
+  };
+
+  const handleCategorias = (selected) => {
+    setCategoria(selected);
+  };
+
+  const handleTipo = (selected) => {
+    setTipo(selected);
+  };
+
+  const handleGenero = (selected) => {
+    setGenero(selected);
   };
 
   const handleFileChange = (e) => {
@@ -35,142 +88,115 @@ const CadastroProduto = () => {
 
   const enviarFormulario = async (e) => {
     e.preventDefault();
-  
+
     const formDataImagens = new FormData();
 
     // Adiciona as fotos ao FormData usando o mesmo nome que o servidor espera ('images')
     for (let i = 0; i < fotos.length; i++) {
-      formDataImagens.append('images', fotos[i]);
+      formDataImagens.append("images", fotos[i]);
     }
-  
+
     // Rota axios para enviar apenas as imagens
     try {
-      const responseImagens = await axios.post('https://localhost:3001/imagem/inserir', formDataImagens, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+      const cadastroImagem = await api.post("/imagem",
+        formDataImagens,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      const urlImage_1 = cadastroImagem.data[0];
+      const urlImage_2 = cadastroImagem.data[1];
+      const urlImage_3 = cadastroImagem.data[2];
+
+      // Cria um map com os valores dos tamanhos
+      const tamanhosSelecionados = tamanho.map((option) => option.value);
+     
+      cadastrar_produto(nome,preco,genero,descricao,tamanhosSelecionados,cor,tipo,urlImage_1,urlImage_2,urlImage_3,categoria)
+      
+    } catch (err) {
+      Swal.fire({
+        title: err,
+        icon: "warning",
+        confirmButtonText: "OK",
       });
-  
-      console.log('Resposta Imagens:', responseImagens);
-  
-      // Se necessário, adicione lógica para extrair IDs ou outras informações relevantes da resposta da primeira rota
-  
-      // Agora, você pode criar um segundo FormData para os outros dados que você deseja enviar para a outra rota
-      const formDataOutrosDados = new FormData();
-      formDataOutrosDados.append('nome', nome);
-      formDataOutrosDados.append('preco', preco);
-      // Adicione outros campos conforme necessário
-  
-      // Rota axios para enviar os outros dados
-      const responseOutrosDados = await axios.post('sua-outra-rota', formDataOutrosDados, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      console.log('Resposta Outros Dados:', responseOutrosDados);
-  
-    } catch (error) {
-      console.error('Erro:', error);
     }
   };
 
   return (
-    <>
-      <div >
-        <Header/>
-        <div className="body-cad">
-          <div className="card-container">
-            <form onSubmit={enviarFormulario}>
-            <div className="parte1">
-              <div className="um">
-                <input
-                  id="idnome"
-                  placeholder="Nome"
-                  name="nome"
-                  value={nome}
-                  onChange={(e) => setNome(e.target.value)}
-                />
+    <div id="formulario-produto-div">
+      <Header />
 
-                <input
-                  placeholder="Preço"
-                  name="preco"
-                  value={preco}
-                  onChange={(e) => setPreco(e.target.value)}
-                />
-              </div>
+      <form onSubmit={enviarFormulario}>
+        <input
+          id="idnome"
+          placeholder="Nome"
+          name="nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
 
-              <div className="dois">
-                <input
-                  placeholder="Cor"
-                  name="cor"
-                  value={cor}
-                  onChange={(e) => setCor(e.target.value)}
-                />
+        <input
+          placeholder="Preço"
+          name="preco"
+          value={preco}
+          onChange={(e) => setPreco(e.target.value)}
+        />
 
-                <select
-                  className="gen"
-                  name="genero"
-                  value={genero}
-                  onChange={(e) => setGenero(e.target.value)}
-                >
-                  <option value="feminino">Feminino</option>
-                  <option value="masculino">Masculino</option>
-                  <option value="unissex">Unissex</option>
-                </select>
-              </div>
-              
-              <div className="tres">
-                <input
-                  placeholder="Tipo"
-                  name="tipo"
-                  value={tipo}
-                  onChange={(e) => setTipo(e.target.value)}
-                />
+        <input
+          placeholder="Cor"
+          name="cor"
+          value={cor}
+          onChange={(e) => setCor(e.target.value)}
+        />
 
-                <Select
-                  className="tam"
-                  isMulti
-                  options={opcoes}
-                  value={selectedOptions}
-                  onChange={handleSelectChange}
-                />
-              </div>
+        <Select options={tipoOptions} value={tipo} onChange={handleTipo} />
 
-            </div>
-            
-            <div className="pat2">
-              <label className="upload">
-                <img  id="icon" src={cam} alt='carregar imagem' />
-                <input
-                  type="file"
-                  id="fotos"
-                  name="fotos"
-                  accept="image/*"
-                  multiple
-                  onChange={handleFileChange}
-                />
-              </label>
-              
+        <Select
+          options={generoOptions}
+          value={genero}
+          onChange={handleGenero}
+        />
 
-              <textarea
-                id="desc"
-                name="descricao"
-                value={descricao}
-                onChange={(e) => setDescricao(e.target.value)}
-                rows={5}
-                style={{ resize: 'none' }}
-                placeholder="descricao"
-              />
-            </div>
-            
+        <Select
+          isMulti
+          options={tamanhoOptions}
+          value={tamanho}
+          onChange={handleTamanho}
+        />
 
-            <button type="submit">Cadastrar</button>
-          </form>
-          </div>
-        </div>
-      </div>
-      
-    </>
+        <Select
+          options={categoriaOpt.map((nome) => ({ value: nome, label: nome }))}
+          value={categoria}
+          onChange={handleCategorias}
+        />
+
+        <label className="upload ">
+          <img id="icon" src={cam} alt="carregar imagem" />
+          <input
+            type="file"
+            id="fotos"
+            name="fotos"
+            accept="image/*"
+            multiple
+            onChange={handleFileChange}
+          />
+        </label>
+        <textarea
+          id="desc"
+          name="descricao"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          rows={5}
+          style={{ resize: "none" }}
+          placeholder="descricao"
+        />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
   );
 };
 
